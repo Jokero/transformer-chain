2d43e78c7a6a4ca1b94a67cc0eff7d402767aebc
DmitryKirilyuk <gk.joker@gmail.com>
2015-09-03 16:14:33 +0300

    dropFirstLevelUndefinedFields option

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-var prepare     = require('./lib/prepare');
-var setDefaults = require('./lib/setDefaults');
-var filter      = require('./lib/filter');
-var validate    = require('./lib/validate');
+var prepare                       = require('./lib/prepare');
+var setDefaults                   = require('./lib/setDefaults');
+var filter                        = require('./lib/filter');
+var validate                      = require('./lib/validate');
+var dropFirstLevelUndefinedFields = require('./lib/dropFirstLevelUndefinedFields');
+var _                             = require('lodash');
 
 /**
  * @param {Object}         object
  * @param {Object|Boolean} config
+ * @param {Object}         [options={}]
+ * @param {Boolean}          [options.dropFirstLevelUndefinedFields=false]
  *
  * @returns {Promise}
  */
-function processParams(object, config) {
+function processParams(object, config, options) {
     if (config === true) {
         return Promise.resolve(object);
     }
 
+    var objectCopy  = _.cloneDeep(object);
+    var configCopy  = _.cloneDeep(config);
+    var optionsCopy = _.cloneDeep(options || {});
+
+    if (!optionsCopy.hasOwnProperty('dropFirstLevelUndefinedFields')) {
+        optionsCopy.dropFirstLevelUndefinedFields = false;
+    }
+
+    if (optionsCopy.dropFirstLevelUndefinedFields) {
+        dropFirstLevelUndefinedFields(objectCopy, configCopy);
+    }
+
     return Promise.resolve().then(function() {
-        var preparedObject = prepare(object, config);
+        var preparedObject = prepare(objectCopy, configCopy, options);
 
-        setDefaults(preparedObject, config);
+        setDefaults(preparedObject, configCopy, options);
 
-        filter(preparedObject, config);
+        filter(preparedObject, configCopy, options);
 
-        return validate(preparedObject, config);
+        return validate(preparedObject, configCopy, options);
     });
 }
 
